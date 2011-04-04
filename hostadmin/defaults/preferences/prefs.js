124c64fd1e3385fb552c8d2e42ac5a801586fc23
farmer1992 <farmer1992@gmail.com>
2011-04-04 06:24:59 +0000

    备份dll

--- hostadmin/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.hostadmin.boolpref", false);
-pref("extensions.hostadmin.intpref", 0);
-pref("extensions.hostadmin.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.{bd54afa8-b14a-4d7a-aecf-37e34e882796}.description", "chrome://hostadmin/locale/overlay.properties");

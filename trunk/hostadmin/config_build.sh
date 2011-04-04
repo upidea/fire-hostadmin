7ab85411ad525a045f728678632c58063088b637
farmer1992@gmail.com <farmer1992@gmail.com@5a1c01d0-06fd-11df-956f-01f849d29158>
2011-04-04 05:45:41 +0000

    修订升级1.1.1

--- trunk/hostadmin/config_build.sh
+++ trunk/hostadmin/config_build.sh
@@ -7,4 +7,4 @@ CHROME_PROVIDERS="chrome/content chrome/locale chrome/skin"
 CLEAN_UP=1
 ROOT_DIRS="components/ defaults/"
 BEFORE_BUILD=
-AFTER_BUILD=
+AFTER_BUILD="mv hostadmin.xpi hostadmin-`grep '<em:version>\(.*\)<\/em:version>' install.rdf  |grep '(\d+\.?)+' -Po`.xpi"

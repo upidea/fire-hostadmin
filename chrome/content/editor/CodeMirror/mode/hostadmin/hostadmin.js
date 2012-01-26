8d29e657d91cefa3b8428ee3a92c62c84e2c4455
farmer1992 <farmer1992@gmail.com>
2012-01-26 18:23:47 +0000

    ipv6 and confirm

--- chrome/content/editor/CodeMirror/mode/hostadmin/hostadmin.js
+++ chrome/content/editor/CodeMirror/mode/hostadmin/hostadmin.js
@@ -1,5 +1,6 @@
 CodeMirror.defineMode("hostadmin", function(config, parserConfig) {
-	var ipv4 = /^((1?\d?\d|(2([0-4]\d|5[0-5])))\.){3}(1?\d?\d|(2([0-4]\d|5[0-5])))/;
+	var ipv4 = /\s*((1?\d?\d|(2([0-4]\d|5[0-5])))\.){3}(1?\d?\d|(2([0-4]\d|5[0-5])))/;
+	var ipv6 = /\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?/ 
 
 	return {
 		startState: function() {return {
@@ -18,7 +19,7 @@ CodeMirror.defineMode("hostadmin", function(config, parserConfig) {
 				}
 				
 				stream.eatWhile(/[#\s]/);
-				if(!state.hasIP && stream.match(ipv4, false)){
+				if(!state.hasIP && (stream.match(ipv4, false) || stream.match(ipv6, false))){
 					state.hasIP = true;
 					return "keyword";
 				}
@@ -33,7 +34,7 @@ CodeMirror.defineMode("hostadmin", function(config, parserConfig) {
 				return "comment";
 			}
 
-			if(stream.match(ipv4)){
+			if(stream.match(ipv4) || stream.match(ipv6)){
 				return "number";
 			}
 

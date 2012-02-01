ed3ebaeced77afc1904a14b2ecfb18a1c1b006f5
farmer1992@gmail.com <farmer1992@gmail.com@5a1c01d0-06fd-11df-956f-01f849d29158>
2012-02-01 04:12:35 +0000

    build script update

--- trunk/config_build.sh
+++ trunk/config_build.sh
@@ -5,6 +5,6 @@
 APP_NAME=hostadmin
 CHROME_PROVIDERS="chrome/content chrome/locale chrome/skin"
 CLEAN_UP=1
-ROOT_DIRS="defaults/"
+ROOT_DIRS="defaults/ modules/"
 BEFORE_BUILD=
 AFTER_BUILD="mv hostadmin.xpi hostadmin-`grep '<em:version>\(.*\)<\/em:version>' install.rdf  |grep '(\d+\.?)+' -Po`.xpi"

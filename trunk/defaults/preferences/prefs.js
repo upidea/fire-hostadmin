e0af41bee4902ba34d4c4ad7a01573a8fdceaf4f
farmer1992@gmail.com <farmer1992@gmail.com@5a1c01d0-06fd-11df-956f-01f849d29158>
2012-04-16 10:50:33 +0000

    config charset

--- trunk/defaults/preferences/prefs.js
+++ trunk/defaults/preferences/prefs.js
@@ -1,4 +1,5 @@
 pref("extensions.hostadmin.hostsfilepath", "default");
+pref("extensions.hostadmin.charset", "auto");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.{bd54afa8-b14a-4d7a-aecf-37e34e882796}.description", "chrome://hostadmin/locale/overlay.properties");
